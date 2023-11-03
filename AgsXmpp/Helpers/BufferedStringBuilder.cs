32380f481727e80a3f85031d7e3d5047582aa7b0
Nathan Ferreira <14365254+nathan130200@users.noreply.github.com>
2023-11-03 13:40:12 -0300

    initial commit

---
+++ AgsXmpp/Helpers/BufferedStringBuilder.cs
@@ -0,0 +1,69 @@
+﻿using System.Buffers;
+using System.Text;
+
+namespace AgsXmpp.Helpers;
+
+public class BufferedStringBuilder : IDisposable
+{
+    private Encoding _encoding;
+    private ArrayBufferWriter<byte> _writer;
+
+    public BufferedStringBuilder(Encoding encoding = default)
+        => Init(encoding);
+
+    public void Dispose()
+    {
+        _writer?.Clear();
+        _writer = null;
+    }
+
+    void Init(Encoding enc)
+    {
+        _encoding = enc ?? Encoding.UTF8;
+        _writer ??= new ArrayBufferWriter<byte>(256);
+    }
+
+    ~BufferedStringBuilder()
+    {
+        Reset();
+    }
+
+    void Reset()
+    {
+        _writer?.Clear();
+        _writer = default;
+    }
+
+    public BufferedStringBuilder AppendLine()
+        => Append('\n');
+
+    public BufferedStringBuilder AppendLine(string str)
+        => Append('\n').Append(str);
+
+    public BufferedStringBuilder Append(char ch)
+        => AppendChars(ch);
+
+    public BufferedStringBuilder Append(string str)
+        => AppendChars(str);
+
+    public BufferedStringBuilder Append(StringBuilder sb)
+        => AppendChars(sb.ToString());
+
+    BufferedStringBuilder AppendChars(params char[] chars)
+    {
+        _encoding.GetBytes(chars, _writer);
+        return this;
+    }
+
+    BufferedStringBuilder AppendChars(ReadOnlySpan<char> chars)
+    {
+        _encoding.GetBytes(chars, _writer);
+        return this;
+    }
+
+    public override string ToString()
+        => _encoding.GetString(_writer.WrittenSpan);
+
+    public override int GetHashCode()
+        => _writer.GetHashCode();
+}
---
+++ AgsXmpp/Jid.cs
@@ -0,0 +1,110 @@
+﻿using AgsXmpp.Helpers;
+
+namespace AgsXmpp;
+
+public sealed class Jid : IEquatable<Jid>
+{
+    public string User
+    {
+        get;
+        init;
+    }
+
+    public string Server
+    {
+        get;
+        init;
+    }
+
+    public string Resource
+    {
+        get;
+        init;
+    }
+
+    public Jid()
+    {
+
+    }
+
+    public Jid(string jid)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(jid);
+
+        var ofs = jid.IndexOf('@');
+
+        if (ofs != -1)
+        {
+            User = jid[0..ofs];
+            jid = jid[1..];
+        }
+
+        ofs = jid.IndexOf('/');
+
+        if (ofs == -1)
+            Server = jid;
+        else
+        {
+            Server = jid[0..ofs];
+            Resource = jid[(ofs + 1)..];
+        }
+    }
+
+    public Jid(string user = default, string server = default, string resource = default)
+    {
+        User = user;
+        Server = server;
+        Resource = resource;
+    }
+
+    public override int GetHashCode()
+        => HashCode.Combine(User ?? string.Empty,
+            Server ?? string.Empty,
+            Resource ?? string.Empty);
+
+    const StringComparison strCompareMethod = StringComparison.Ordinal;
+
+    public bool Equals(Jid other)
+    {
+        if (ReferenceEquals(other, null))
+            return false;
+
+        if (ReferenceEquals(other, this))
+            return true;
+
+        bool result =
+            string.Compare(User, other.User, strCompareMethod) == 0 &&
+            string.Compare(Server, other.Server, strCompareMethod) == 0;
+
+        if (IsBare)
+            return result;
+
+        return result && string.Compare(Resource, other.Resource, strCompareMethod) == 0;
+    }
+
+    public bool IsBare
+        => string.IsNullOrEmpty(Resource);
+
+    public bool IsFull
+        => !IsBare;
+
+    public Jid Bare
+        => new(user: User, server: Server);
+
+    public override string ToString()
+    {
+        using (var sb = new BufferedStringBuilder())
+        {
+            if (User is not null)
+                sb.Append(User).Append('@');
+
+            if (Server is not null)
+                sb.Append(Server);
+
+            if (Resource is not null)
+                sb.Append('/').Append(Resource);
+
+            return sb.ToString();
+        }
+    }
+}
